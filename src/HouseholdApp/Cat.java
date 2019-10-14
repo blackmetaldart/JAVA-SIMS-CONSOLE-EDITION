f6e36e496c656fd4c12741cfa19b436319697ef8
Alexander <mrbeachcode@gmail.com>
2019-10-14 19:41:41 -0400

    Fixed up the cat class.

--- src/HouseholdApp/Cat.java
+++ src/HouseholdApp/Cat.java
@@ -4,16 +4,16 @@ public class Cat extends Pet {
 
     @Override
     public void feedPet(){
-        System.out.println("Cat just ate its food." );
+        System.out.println("The cat just ate its food." );
     }
 
     @Override
     public void groomPet(){
-        System.out.println("Cat is so clean now!");
+        System.out.println("The cat is so clean now!");
     }
 
     @Override
     public void playWithPet(){
-        System.out.println("Cat just played with laser pointer!");
+        System.out.println("The cat just played with the laser pointer!");
     }
 }
